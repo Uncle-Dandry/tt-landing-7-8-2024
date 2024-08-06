75f3a16b416bd2cef82a2b31a679148ed8c6d640
Uncle <russkienjn@gmail.com>
2024-08-07 01:51:20 +0300

    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+import App from './App';
+
+import './index.css';
+
+const root = document.getElementById('root') as HTMLElement;
+
+createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
