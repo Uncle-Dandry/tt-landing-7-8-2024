1ebe7df5fc5090a1108ad7bbea5443fcfe3225d7
Uncle <russkienjn@gmail.com>
2024-08-07 10:09:35 +0300

    add eslint for dev

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ import logo from './logo.svg';
 
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>
+            src/App.tsx
+          </code>
+          {' '}
+          and save to reload.
         </p>
-        
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -25,6 +31,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
